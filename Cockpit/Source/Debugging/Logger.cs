9893284ae51343612004892e6467c1d620d89b22
Andrea Milanta <andrea.milanta@mail.polimi.it>
2017-02-11 15:44:51 +0100

    Logger Singleton Partial Creation

---
+++ Cockpit/Source/Debugging/Logger.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Logger for the whole project.
+ * It is a singleton
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cockpit.Source.Debugging
+{
+    public class Logger
+    {
+        private static Logger instance = new Logger();
+
+        private Logger()
+        {
+            //Create / Opens a log file
+        }
+
+        //Singoleton implementation
+        public static Logger Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
